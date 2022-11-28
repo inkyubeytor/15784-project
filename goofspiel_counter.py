191c44b9fffead13a5cadf36e3cdd0c16dbb0d43
Abhishek Vijayakumar <vijayakumar.abhishek@gmail.com>
2022-11-28 17:21:55 -0500

    state counter for goofspiel

--- goofspiel_counter.py
+++ goofspiel_counter.py
@@ -17,11 +17,19 @@ flags.DEFINE_integer("players", 2, "Number of players")
 flags.DEFINE_integer("print_freq", 10,
                      "How often to print the exploitability")
 
+def state_count(game):
+    root = game.new_initial_state()
+    def state_count_helper(state):
+        return 1 + sum(state_count_helper(state.child(a)) for a in state.legal_actions(state.current_player()))
+
+    return state_count_helper(root)
 
 def main(_):
     game = pyspiel.load_game(FLAGS.game)
+    print(f"Game tree size: {state_count(game)}")
     state_counter = PCFRSolver(game)
-    print(state_counter._current_policy.action_probability_array.shape)
+    print(f"Infoset policy shape: {state_counter._current_policy.action_probability_array.shape}")
+
 
 if __name__ == "__main__":
     app.run(main)
