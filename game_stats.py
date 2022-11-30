1d47569583ae8dfa14c8b4dc8ac1645cfa09aab7
Abhishek Vijayakumar <vijayakumar.abhishek@gmail.com>
2022-11-30 12:55:24 -0500

    counting states goofspiel

--- game_stats.py
+++ game_stats.py
@@ -11,7 +11,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_integer("iterations", 10, "Number of iterations")
 flags.DEFINE_string(
     "game",
-    "python_goofspiel(num_cards=3,num_turns=3)",
+    "python_goofspiel(num_cards=5,num_turns=4)",
     # "turn_based_simultaneous_game(game=goofspiel(imp_info=False,num_cards=3,players=2,points_order=random))",
     "Name of the game")
 flags.DEFINE_integer("players", 2, "Number of players")
@@ -54,21 +54,36 @@ def str_collision(game):
         s = queue.pop()
         if s.current_player() != pyspiel.PlayerId.TERMINAL:
             info = repr(s)
-            print(str(s))
-            if info not in strs:
-                if s.current_player() != pyspiel.PlayerId.CHANCE:
-                    strs.add(info)
+            if s.current_player() != pyspiel.PlayerId.CHANCE:
+                strs.add(info)
             for a in s.legal_actions(s.current_player()):
                 queue.append(s.child(a))
     return len(strs)
 
+
+def str_collision_all(game):
+    """Counts the number of unique state string representations."""
+    strs = set()
+    r = game.new_initial_state()
+    queue = [r]
+    while queue:
+        s = queue.pop()
+        info = repr(s)
+        if info not in strs:
+            strs.add(info)
+        for a in s.legal_actions(s.current_player()):
+            queue.append(s.child(a))
+    return len(strs)
+
 def main(_):
     game = pyspiel.load_game(FLAGS.game)
-    print(f"Game tree size: {state_count(game)}")
-    print(f"Number of unique state strings: {str_collision(game)}")
-    print(f"A state is {state_size(game)} bytes")
-    # state_counter = PCFRSolver(game)
-    # print(f"Infoset policy shape: {state_counter._current_policy.action_probability_array.shape}")
+    # print(f"Game tree size (all nodes) {state_count_all(game)}")
+    # print(f"Game tree size (player nodes): {state_count(game)}")
+    print(f"Number of unique state strings (all nodes): {str_collision_all(game)}")
+    print(f"Number of unique state strings (player nodes): {str_collision(game)}")
+    # print(f"A state is {state_size(game)} bytes")
+    state_counter = PCFRSolver(game)
+    print(f"Infoset policy shape: {state_counter._current_policy.action_probability_array.shape}")
 
 
 if __name__ == "__main__":
