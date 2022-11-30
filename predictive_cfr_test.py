f4df34da088ff1df35077f84b43ce3b7f322cfa9
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2022-11-29 22:43:47 -0500

    memory profiling

--- predictive_cfr_test.py
+++ predictive_cfr_test.py
@@ -7,16 +7,17 @@ from predictive_cfr import PCFRSolver
 
 from goofspiel import *
 from exploitability import *
+import resource
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer("iterations", 30, "Number of iterations")
+flags.DEFINE_integer("iterations", 1, "Number of iterations")
 flags.DEFINE_string(
     "game",
-    "python_goofspiel(num_cards=4,num_turns=4)",
+    "python_goofspiel(num_cards=4,num_turns=3)",
     "Name of the game")
 flags.DEFINE_integer("players", 2, "Number of players")
-flags.DEFINE_integer("print_freq", 10,
+flags.DEFINE_integer("print_freq", -1,
                      "How often to print the exploitability")
 
 
@@ -53,10 +54,14 @@ def main(_):
     print(f"First iter time: {first_iter_time}")
     print(f"Print iter average: {print_iter_time / print_iter_count if print_iter_count else 0}")
     print(f"No Print iter average: {noprint_iter_time / noprint_iter_count if noprint_iter_count else 0}")
+    print(f"Peak memory usage (KB): {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss}")
 
 
 if __name__ == "__main__":
+
     app.run(main)
+
+
     # cProfile.run("app.run(main)", "py_3_3_stats")
     #
     # import pstats
